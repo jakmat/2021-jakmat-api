0839bf56c9989afb029e9e08e2a40891efc16d64
jakmat <jakubmatusiak87@gmail.com>
2022-08-21 15:47:22 +0200

    WIP

---
+++ scripts/sites-available.deploy.sh
@@ -0,0 +1,6 @@
+scp ../proxy/nginx/sites-available/* root@185.201.115.103:/etc/nginx/sites-available
+ssh root@185.201.115.103 "
+  sudo systemctl restart nginx;
+  sudo systemctl restart api;
+  curl http://127.0.0.1/
+"
